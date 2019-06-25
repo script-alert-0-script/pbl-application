c09dd89561461bfde600b9c8c32e972f418b2f30
amamama <amatsu.t.aa@m.titech.ac.jp>
2019-06-25 15:35:13 +0900

    impl getItem

---
+++ src/main/resources/static/fetch.js
@@ -0,0 +1,14 @@
+async function getAllItems() {
+    return (await fetch('api/item')).json();
+}
+
+async function getItem(id) {
+    return (await fetch(`api/item/${id}`)).json();
+}
+
+async function getItemSearch(q) {
+    let param = new URLSearchParams();
+    param.set('name', q);
+    return (await fetch('api/item/search' + param.toString())).json();
+}
+
