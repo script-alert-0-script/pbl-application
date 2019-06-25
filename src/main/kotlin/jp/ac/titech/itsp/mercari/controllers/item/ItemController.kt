4ecef233f241c877760c2a279feb9b00c77e4e36
Ryo Kuramoto <kuramoto@se.cs.titech.ac.jp>
2019-06-25 13:58:14 +0900

    enable swagger-ui

--- src/main/kotlin/jp/ac/titech/itsp/mercari/controllers/item/ItemController.kt
+++ src/main/kotlin/jp/ac/titech/itsp/mercari/controllers/item/ItemController.kt
@@ -6,6 +6,13 @@ import jp.ac.titech.itsp.mercari.services.ItemService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE
+import org.springframework.web.bind.annotation.GetMapping
+import io.swagger.annotations.ApiResponse
+import io.swagger.annotations.ApiResponses
+import io.swagger.annotations.ApiOperation
+
 
 @RestController
 @RequestMapping("/api/item")
@@ -14,24 +21,29 @@ class ItemController {
     @Autowired
     lateinit var itemService: ItemService
 
+    @ApiOperation("Register a item")
     @PostMapping
     fun register(@RequestParam name: String, @RequestParam user: String): ResponseEntity<Long> {
         val item = itemService.create(name, user)
         return ResponseEntity.ok(item.id)
     }
 
+    @ApiOperation("Get a item by id")
+    @ApiResponses(value = [ApiResponse(code = 404, message = "Item not found")])
     @GetMapping("/{id}")
     fun get(@PathVariable("id") id: Long): ResponseEntity<Item> {
         return try {
             ResponseEntity.ok(itemService.get(id))
         } catch (e: NotFoundException) {
-            ResponseEntity.notFound().build<Item>()
+            ResponseEntity.notFound().build()
         }
     }
 
+    @ApiOperation("Get all items")
     @GetMapping
     fun getAll() = ResponseEntity.ok(itemService.getAll())
 
+    @ApiOperation("Search items by name")
     @GetMapping("/search")
     fun search(@RequestParam name: String) = ResponseEntity.ok(itemService.search(name))
 
