8d136636b187f0de857298dacc3c06a6e0abbfd6
amamama <amatsu.t.aa@m.titech.ac.jp>
2019-07-02 16:16:29 +0900

    impl item page

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,9 +1,18 @@
-new Vue({
-    el: '#app',
-    data: {
+const Index = {
+    template: `
+<div id="index">
+    <input v-model="param">
+    <ul>
+        <li v-for="item in items">
+            <router-link v-bind:to="\`/item/\${item.id}\`">{{ item.id }} - {{ item.name }} - {{ item.price }}</router-link>
+        </li>
+    </ul>
+</div>
+`,
+    data: /*{
         items: [],
         param: ''
-    },
+    }*/ () => ({items: [], param: ''}),
     async created() {
         // インスタンス作成時に自動的にローカルストレージをfetchする
         this.items = await getAllItems();
@@ -22,5 +31,18 @@ new Vue({
             this.items = await getItemSearch(this.param);
         }
     }
-});
+};
+
+const Item = {
+    template: '<div>item page</div>'
+}
+
+const routes = [
+    {path: '/', component: Index},
+    {path: '/item/:id', component: Item}
+]
+
+const router = new VueRouter({routes});
+
+const app = new Vue({router}).$mount("#app");
 
