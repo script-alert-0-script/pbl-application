b40ee01347e7e7985cecab76d1fff0bfac60f9f9
Ryo Kuramoto <kuramoto@se.cs.titech.ac.jp>
2019-07-09 13:57:00 +0900

    fix password encode

--- src/main/kotlin/jp/ac/titech/itsp/mercari/configuration/DataConfiguration.kt
+++ src/main/kotlin/jp/ac/titech/itsp/mercari/configuration/DataConfiguration.kt
@@ -3,13 +3,14 @@ package jp.ac.titech.itsp.mercari.configuration
 import jp.ac.titech.itsp.mercari.models.Item
 import jp.ac.titech.itsp.mercari.models.User
 import jp.ac.titech.itsp.mercari.repositories.ItemRepository
+import jp.ac.titech.itsp.mercari.repositories.UserRepository
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.InitializingBean
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
-import jp.ac.titech.itsp.mercari.repositories.UserRepository
-import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.crypto.password.PasswordEncoder
 
 @Configuration
 class DataConfiguration {
@@ -24,12 +25,12 @@ class DataConfiguration {
     // TODO
     @Profile("dev")
     @Bean
-    fun init(): InitializingBean = InitializingBean {
-        userRepository.save(User("default", "pass")).run {
+    fun init(passwordEncoder: PasswordEncoder): InitializingBean = InitializingBean {
+        userRepository.save(User("default", passwordEncoder.encode("pass"))).run {
             logger.debug("User saved $id:$password")
         }
         repeat(10) {
-            userRepository.save(User("user$it", "pass")).run {
+            userRepository.save(User("user$it", passwordEncoder.encode("pass"))).run {
                 itemRepository.save(Item("item-name$it", this))
                 logger.debug("User saved $id:$password")
             }
