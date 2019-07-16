76bb74f2991b5e0301456bc6ffbda0ebcadd7bf1
amamama <amatsu.t.aa@m.titech.ac.jp>
2019-07-16 16:37:37 +0900

    fix typo

--- src/main/resources/static/fetch.js
+++ src/main/resources/static/fetch.js
@@ -25,13 +25,13 @@ async function post(uri, formData) {
 }
 
 async function postRequest(id) {
-    return (await post(`api/user/${id}/request`, new FormData()));
+    return (await post(`api/item/${id}/request`, new FormData()));
 }
 
 async function postCancel(id) {
-    return (await post(`api/user/${id}/cancel`, new FormData()));
+    return (await post(`api/item/${id}/cancel`, new FormData()));
 }
 
 async function postAllow(id) {
-    return (await post(`api/user/${id}/allow`, new FormData()));
+    return (await post(`api/item/${id}/allow`, new FormData()));
 }
