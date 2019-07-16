312a57f989b869b82af7eb2b60d6dcb786c3cce4
Ryo Kuramoto <kuramoto@se.cs.titech.ac.jp>
2019-07-16 16:23:30 +0900

    update tests

--- src/test/kotlin/jp/ac/titech/itsp/mercari/controllers/ItemControllerTests.kt
+++ src/test/kotlin/jp/ac/titech/itsp/mercari/controllers/ItemControllerTests.kt
@@ -3,11 +3,11 @@ package jp.ac.titech.itsp.mercari.controllers
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import jp.ac.titech.itsp.mercari.models.Item
-import jp.ac.titech.itsp.mercari.models.ItemState
 import jp.ac.titech.itsp.mercari.models.User
 import jp.ac.titech.itsp.mercari.repositories.ItemRepository
 import jp.ac.titech.itsp.mercari.repositories.UserRepository
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -36,30 +36,36 @@ class ItemControllerTests {
     @Autowired
     lateinit var mvc: MockMvc
 
+    lateinit var user: User
+
     @BeforeEach
     fun before() {
-        val user = userRepository.save(User("user"))
-        itemRepository.saveAll((1..5L).map { Item("name$it", user) })
+        user = userRepository.save(User("user"))
     }
 
     @Test
     @WithMockUser
     fun registerItem() {
-        mvc.perform(
+        val id = mvc.perform(
             post("/api/item")
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .content("name=hoge")
         )
             .andExpect(status().isOk)
+            .andReturn().response.contentAsString.toLong()
+        assertTrue(itemRepository.existsById(id))
+        assertEquals("hoge", itemRepository.getOne(id).name)
+
     }
 
     @Test
     fun getItem() {
-        val result = mvc.perform(get("/api/item/1"))
+        val item = itemRepository.save(Item("item", user))
+        val result = mvc.perform(get("/api/item/${item.id}"))
             .andExpect(status().isOk)
             .andReturn()
         val actual: Item = jacksonObjectMapper().readValue(result.response.contentAsString)
-        assertEquals(ItemState.AVAILABLE, actual.state)
+        assertEquals(item, actual)
     }
 
     @Test
@@ -70,18 +76,23 @@ class ItemControllerTests {
 
     @Test
     fun getItems() {
+        repeat(5) { itemRepository.save(Item("item $it", user)) }
         val result = mvc.perform(get("/api/item"))
             .andExpect(status().isOk)
             .andReturn()
         val actual: List<Item> = jacksonObjectMapper().readValue(result.response.contentAsString)
+        assertEquals(itemRepository.count(), actual.size.toLong())
     }
 
     @Test
     fun searchItem() {
-        val result = mvc.perform(get("/api/item/search?name=am"))
+        itemRepository.save(Item("searchable item", user))
+        // TODO url encode
+        val result = mvc.perform(get("/api/item/search?name=le it"))
             .andExpect(status().isOk)
             .andReturn()
         val actual: List<Item> = jacksonObjectMapper().readValue(result.response.contentAsString)
+        assertEquals(1, actual.size)
     }
 
 }
