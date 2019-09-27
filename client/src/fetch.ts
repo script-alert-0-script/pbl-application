d016dd528bbe41dbf7084e2b8ccb0ac409d23619
amamama <amatsu.t.aa@m.titech.ac.jp>
2019-09-27 14:29:25 +0900

    typing fetch.ts

--- client/src/fetch.ts
+++ client/src/fetch.ts
@@ -1,4 +1,4 @@
-async function fetchJSON(uri) {
+async function fetchJSON(uri: string) {
     return (await fetch(uri)).json();
 }
 
@@ -6,32 +6,32 @@ export async function getAllItems() {
     return await fetchJSON('/api/item');
 }
 
-export async function getItem(id) {
+export async function getItem(id: number) {
     return await fetchJSON(`/api/item/${id}`);
 }
 
-export async function getItemSearch(q) {
+export async function getItemSearch(q: string) {
     let param = new URLSearchParams();
     param.set('name', q);
     return await fetchJSON('/api/item/search?' + param.toString());
 }
 
-export async function getUser(id) {
+export async function getUser(id: number) {
     return await fetchJSON(`/api/user/${id}`);
 }
 
-async function post(uri, formData) {
-    return await fetch(uri, {method: "POST", body: formData});
+async function post(uri: string, formData: FormData) {
+    return await fetch(uri, {method: 'POST', body: formData});
 }
 
-export async function postRequest(id) {
+export async function postRequest(id: number) {
     return (await post(`/api/item/${id}/request`, new FormData()));
 }
 
-export async function postCancel(id) {
+export async function postCancel(id: number) {
     return (await post(`/api/item/${id}/cancel`, new FormData()));
 }
 
-export async function postAllow(id) {
+export async function postAllow(id: number) {
     return (await post(`/api/item/${id}/allow`, new FormData()));
 }
