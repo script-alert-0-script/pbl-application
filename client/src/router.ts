49e69c4722fc3f654834f0aa9182c4cd7d2550f3
amamama <amatsu.t.aa@m.titech.ac.jp>
2019-09-27 14:09:10 +0900

    send message to send-message handler

--- client/src/router.ts
+++ client/src/router.ts
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import ItemPage from "./views/ItemPage.vue"
 
 Vue.use(Router);
 
@@ -19,8 +20,7 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () =>
-          import("./views/ItemPage.vue")
+      component: ItemPage
     }
   ]
 });
