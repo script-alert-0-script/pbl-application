78c78e3a87f59d83c065c8de65fb253673b2b933
Ryo Kuramoto <kuramoto@se.cs.titech.ac.jp>
2019-10-18 10:14:36 +0900

    Upgrade vue-cli v3 -> v4

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,11 +1,10 @@
 import Vue from "vue";
-import Router from "vue-router";
-import Home from "./views/Home.vue";
-import ItemPage from "./views/ItemPage.vue";
+import VueRouter from "vue-router";
+import Home from "@/views/Home.vue";
 
-Vue.use(Router);
+Vue.use(VueRouter);
 
-export default new Router({
+export default new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
@@ -20,7 +19,8 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: ItemPage
+      component: () =>
+        import(/* webpackChunkName: "item" */ "@/views/ItemPage.vue")
     }
   ]
 });
