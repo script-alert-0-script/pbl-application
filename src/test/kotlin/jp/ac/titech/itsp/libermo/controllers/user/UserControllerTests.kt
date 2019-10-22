8c8c05036355b585ec90fe1a3648ac4ef98a6658
Ryo Kuramoto <kuramoto@se.cs.titech.ac.jp>
2019-10-22 23:02:21 +0900

    Fix failing tests

--- src/test/kotlin/jp/ac/titech/itsp/libermo/controllers/user/UserControllerTests.kt
+++ src/test/kotlin/jp/ac/titech/itsp/libermo/controllers/user/UserControllerTests.kt
@@ -5,17 +5,17 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import jp.ac.titech.itsp.libermo.models.User
 import jp.ac.titech.itsp.libermo.repositories.UserRepository
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.http.MediaType
+import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @ExtendWith(SpringExtension::class)
@@ -29,23 +29,18 @@ class UserControllerTests {
     @Autowired
     private lateinit var mvc: MockMvc
 
-    @Test
-    fun registerUser() {
-        val result = mvc.perform(
-            post("/api/user")
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .content("id=my-id&name=my-name&password=my-pass")
-        )
-            .andExpect(status().isOk)
-            .andReturn()
-        val actual: User = jacksonObjectMapper().readValue(result.response.contentAsString)
-        assertEquals("my-id", actual.id)
+    @BeforeEach
+    fun before() {
+        userRepository.save(User(TEST_ID, TEST_EMAIL))
     }
 
     @Test
+    @WithMockUser(TEST_ID)
     fun getUser() {
-        val user = userRepository.save(User("test-id", "test-name", "test-pass"))
-        val result = mvc.perform(get("/api/user/test-id"))
+        val user = userRepository.save(User("test-id"))
+        val result = mvc.perform(
+            get("/api/user/test-id")
+        )
             .andExpect(status().isOk)
             .andReturn()
         val actual: User = jacksonObjectMapper().readValue(result.response.contentAsString)
@@ -53,9 +48,27 @@ class UserControllerTests {
     }
 
     @Test
+    @WithMockUser(TEST_ID)
     fun getUserNotFound() {
         mvc.perform(get("/api/user/not-exist"))
             .andExpect(status().isNotFound)
+    }
+
+    @Test
+    @WithMockUser(TEST_ID)
+    fun getMe() {
+        val result = mvc.perform(
+            get("/api/user/me")
+        )
+            .andExpect(status().isOk)
+            .andReturn()
+        val actual: User = jacksonObjectMapper().readValue(result.response.contentAsString)
+        assertEquals(TEST_ID, actual.id)
+    }
+
+    companion object {
+        const val TEST_ID = "test-user"
+        const val TEST_EMAIL = "test@m.titech.ac.jp"
     }
 
 }
