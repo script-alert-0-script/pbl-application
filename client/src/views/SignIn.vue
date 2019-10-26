bf4eb23aa9c84d8c2ff5df7a1fdf2b702f5aef83
malony_0 <hamuta57@yahoo.co.jp>
2019-10-26 18:27:21 +0900

    Impl modal for signin

--- client/src/views/SignIn.vue
+++ client/src/views/SignIn.vue
@@ -1,43 +1,18 @@
 <template>
-  <div id="signin">
-    <h2>ログイン</h2>
-    <!-- TODO: メールアドレスがmアドレスだけだと分かりやすいように -->
-    <form @submit.prevent="signIn">
-      <input type="email" placeholder="メールアドレス" v-model="email" />
-      <input type="password" placeholder="パスワード" v-model="password" />
-      <button type="submit">ログイン</button>
-    </form>
-    <p>
-      アカウントをお持ちでない方は
-      <router-link to="/signup">新規登録</router-link>
-    </p>
-    <p>
-      {{ message }}
-    </p>
-  </div>
+  <v-app>
+    <div id="signin">
+      <sign-in-modal />
+    </div>
+  </v-app>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import { signIn } from "@/auth";
-
-@Component({})
-export default class SignIn extends Vue {
-  email = "";
-  password = "";
-  message = "";
-
-  signIn() {
-    signIn(this.email, this.password)
-      .then(async () => {
-        // TODO: GET /api/user/me
-        this.$router.push("/");
-      })
-      .catch(e => {
-        if (e instanceof Error) {
-          this.message = e.message;
-        }
-      });
+import SignInModal from "@/components/SignInModal.vue";
+@Component({
+  components: {
+    "sign-in-modal": SignInModal
   }
-}
+})
+export default class SignIn extends Vue {}
 </script>
